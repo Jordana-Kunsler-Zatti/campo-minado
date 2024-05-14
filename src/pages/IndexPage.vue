0f7814765e8e68e97f87c83b14f59bb0a072cd9d
Jordana-Kunsler-Zatti <jordana.zatti@gmail.com>
2024-05-14 20:46:03 -0300

    make routes

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -2,12 +2,14 @@
   <q-page class="flex flex-center chess-background">
     <div class="flex flex-center opacity no-wrap column">
       <p class="text-h3 h3 text-center">Bem vindo ao <b>Campo Minado</b></p>
-      <q-btn
-        class="q-mt-md"
-        color="primary"
-        icon-right="arrow_forward"
-        label="Ir para o campo"
-      />
+      <router-link to="/jogo">
+        <q-btn
+          class="q-mt-md"
+          color="primary"
+          icon-right="arrow_forward"
+          label="Ir para o campo"
+        />
+      </router-link>
     </div>
   </q-page>
 </template>
--- src/router/routes.js
+++ src/router/routes.js
@@ -1,18 +1,22 @@
 const routes = [
   {
-    path: '/',
-    component: () => import('layouts/MainLayout.vue'),
-    children: [
-      { path: '', component: () => import('pages/IndexPage.vue') }
-    ]
+    path: "/",
+    name: "",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [{ path: "", component: () => import("pages/IndexPage.vue") }],
   },
-
-  // Always leave this as last one,
-  // but you can also remove it
   {
-    path: '/:catchAll(.*)*',
-    component: () => import('pages/ErrorNotFound.vue')
-  }
-]
+    path: "/jogo",
+    name: "jogo",
+    component: () => import("layouts/MainLayout.vue"),
+    children: [
+      { path: "/jogo", component: () => import("pages/GamePage.vue") },
+    ],
+  },
+  {
+    path: "/:catchAll(.*)*",
+    component: () => import("pages/ErrorNotFound.vue"),
+  },
+];
 
-export default routes
+export default routes;
