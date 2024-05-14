436dc52d48eecc97f48a05cc66b04f5014f6b210
Jordana-Kunsler-Zatti <jordana.zatti@gmail.com>
2024-05-14 20:08:40 -0300

    create home

--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,15 +1,19 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="flex flex-center chess-background">
+    <div class="flex flex-center opacity no-wrap column">
+      <p class="text-h3 h3 text-center">Bem vindo ao <b>Campo Minado</b></p>
+      <q-btn
+        class="q-mt-md"
+        color="primary"
+        icon-right="arrow_forward"
+        label="Ir para o campo"
+      />
+    </div>
   </q-page>
 </template>
 
 <script setup>
 defineOptions({
-  name: 'IndexPage'
+  name: "IndexPage",
 });
 </script>
