d0fb1f5645ffadac2f69011a4f984873ddf30649
Jordana-Kunsler-Zatti <jordana.zatti@gmail.com>
2024-05-21 20:31:55 -0300

    finish modals

--- src/pages/GamePage.vue
+++ src/pages/GamePage.vue
@@ -4,32 +4,52 @@
       <font-awesome-icon class="fa-solid fa-house color-gray-dark" />
     </q-btn>
     <h1>GAME</h1>
-    <!-- <swal /> -->
   </q-page>
 </template>
 
 <script>
 import { defineComponent } from "vue";
+import { swalConfs } from "src/utils/utils-swal";
 import Swal from "sweetalert2";
+
+const gameOverToast = () => {
+  return Swal.fire(
+    swalConfs({
+      title: "Você estourou a bomba!",
+      icon: "fa-circle-xmark",
+      iconColor: "#b31b1b",
+      buttonConfimLabel:
+        "Tente novamente <font-awesome-icon class='fa-solid fa-arrow-rotate-right' />",
+      buttonDenyLabel:
+        "Página inicial <font-awesome-icon class='fa-solid fa-house' />",
+    })
+  ).then((result) => {
+    if (result.isConfirmed) {
+      console.log("Confirmando");
+    } else if (result.isDenied) {
+      console.log("Negado");
+    }
+  });
+};
 
 export default defineComponent({
   name: "GamePage",
   methods: {
     handleClickHome() {
-      Swal.fire({
-        title: `<label class='titleSwal'>Você deseja pausar?</label>`,
-        html: `<label class='subtitleSwal'>Se você desistir no meio da partida, ela não será salva. Mas ao pausar retomará desse ponto. Caso queira continuar basta fechar a modal no “X”</label>`,
-        iconHtml: `<font-awesome-icon class="fa-solid fa-circle-question iconSwal" />`,
-        width: "20em",
-        iconColor: "#ff8333",
-        showCloseButton: true,
-        showDenyButton: true,
-        showCancelButton: false,
-        confirmButtonColor: "#0bb3d9",
-        denyButtonColor: "#dedede",
-        confirmButtonText: `<label>Pausar o jogo <font-awesome-icon class="fa-solid fa-circle-pause" /></label>`,
-        denyButtonText: `<label class="color-gray-dark">Desistir <font-awesome-icon class="fa-solid fa-times" /></label>`,
-      }).then((result) => {
+      Swal.fire(
+        swalConfs({
+          title: "Você deseja pausar?",
+          subtitle:
+            "Se você desistir no meio da partida, ela não será salva. Mas ao pausar retomará desse ponto. Caso queira continuar basta fechar a modal no “X”",
+          icon: "fa-circle-question",
+          iconColor: "#ff8333",
+          showCloseButton: true,
+          buttonConfimLabel:
+            "Pausar o jogo <font-awesome-icon class='fa-solid fa-circle-pause' />",
+          buttonDenyLabel:
+            "Desistir <font-awesome-icon class='fa-solid fa-times' />",
+        })
+      ).then((result) => {
         if (result.isConfirmed) {
           console.log("Confirmando");
         } else if (result.isDenied) {
