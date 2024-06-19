94a2e05ff76c9db88f82b2ce4aedc572ffe068e2
Jordana-Kunsler-Zatti <jordana.zatti@gmail.com>
2024-06-19 20:12:41 -0300

    adjust toggle

--- src/pages/GamePage.vue
+++ src/pages/GamePage.vue
@@ -3,43 +3,39 @@
     <q-btn round color="secondary q-ma-md" @click="handleClickHome()">
       <font-awesome-icon class="fa-solid fa-house color-gray-dark" />
     </q-btn>
-    
+
     <div class="row q-mt-md flex-center">
       <q-card class="flex-center no-box-shadow">
         <q-card-section class="column flex-center">
-          <q-icon name="flag" size="40px" color="red"/>
+          <q-icon name="flag" size="40px" color="red" />
           {{ bombCount }} Bombas
         </q-card-section>
       </q-card>
       <q-card class="flex-center no-box-shadow">
         <q-card-section class="column flex-center">
-          <q-icon name="timer" size="40px" color="blue"/>
-          00:00
+          <q-icon name="timer" size="40px" color="blue" />
+          {{ timer }}
         </q-card-section>
       </q-card>
       <q-card class="flex-center no-box-shadow">
         <q-card-section class="column flex-center">
-          <q-icon name="fa-solid fa-trophy" size="40px" color="yellow"/>
+          <q-icon name="fa-solid fa-trophy" size="40px" color="yellow" />
           1 Campo
         </q-card-section>
       </q-card>
     </div>
 
     <!-- botão para iniciar o jogo e colocar bandeiras -->
-    <div class="row flex-center">
-      <q-btn
-        @click="init()"
-        icon-right="send"
-        v-bind="startButton"
-        class="q-mb-md"
-      />
-  
-      <q-btn
-        round
-        class="absolute button-margin"
-        size="10px"
+    <div class="row flex-center q-mb-md">
+      <q-btn @click="init()" icon-right="send" v-bind="startButton" />
+
+      <q-toggle
+        color="red"
+        v-model="isFlag"
         @click="flagButton()"
-        v-bind="flagButtonProps"
+        checked-icon="flag"
+        unchecked-icon="fa-solid fa-bomb"
+        size="lg"
       />
     </div>
 
@@ -60,17 +56,13 @@
         </div>
       </div>
     </div>
-
-    <!-- imprime os numeros lógicos do tabuleiro na fase de teste, excluir posteriormente  -->
-    <!-- <div id="board" class="flex flex-center"></div> -->
-
   </q-page>
 </template>
 
 <script>
-import { defineComponent, watch } from "vue";
 import { swalConfs } from "src/utils/utils-swal";
 import Swal from "sweetalert2";
+import { defineComponent, ref } from "vue";
 
 const gameOverToast = () => {
   return Swal.fire(
@@ -85,10 +77,10 @@ const gameOverToast = () => {
     })
   ).then((result) => {
     if (result.isConfirmed) {
-      window.location.reload()
+      window.location.reload();
       console.log("Confirmando");
     } else if (result.isDenied) {
-      window.location.href = '/';
+      window.location.href = "/";
       console.log("Negado");
     }
   });
@@ -98,26 +90,40 @@ export default defineComponent({
   name: "GamePage",
   data() {
     return {
+      isFlag: ref(false),
       game: {
         cellAroundingMine: [
-          [-1, -1],[-1, 0],[-1, 1],
-          [ 0, -1]        ,[ 0, 1],
-          [ 1, -1],[ 1, 0],[ 1, 1],
+          [-1, -1],
+          [-1, 0],
+          [-1, 1],
+          [0, -1],
+          [0, 1],
+          [1, -1],
+          [1, 0],
+          [1, 1],
         ],
         boardRowsLength: 12,
         boardColumnsLength: 12,
         minesLength: 13,
         board: [],
         minesPositions: [],
-        tilesRevealed: 0
+        tilesRevealed: 0,
       },
-      disableField:{disable:true, class:'fixed-size'},
-      startButton:{disable:false, label:"Começar", color:"primary"},
-      rowsButtons:[],
-      flagButtonProps:{color:"grey-5", icon: 'flag'},
+      disableField: { disable: true, class: "fixed-size" },
+      startButton: { disable: false, label: "Começar", color: "primary" },
+      rowsButtons: [],
+      flagButtonProps: { color: "grey-5", icon: "flag" },
       flagSwitch: false,
-      iconNumbers: ['','fa-solid fa-1','fa-solid fa-2','fa-solid fa-3','fa-solid fa-4','fa-solid fa-5'],
+      iconNumbers: [
+        "",
+        "fa-solid fa-1",
+        "fa-solid fa-2",
+        "fa-solid fa-3",
+        "fa-solid fa-4",
+        "fa-solid fa-5",
+      ],
       bombCount: 0,
+      timer: "00:00",
     };
   },
 
@@ -131,14 +137,21 @@ export default defineComponent({
     createButtons() {
       for (let j = 0; j < this.game.boardRowsLength; j++) {
         this.rowsButtons.push({
-          id:j,
-          buttons:[]
-        })
+          id: j,
+          buttons: [],
+        });
         for (let i = 0; i < this.game.boardColumnsLength; i++) {
           this.rowsButtons[j].buttons.push({
             id: i,
-            icon: '',
-            color: j % 2 === 0 ? i % 2 === 0 ? 'bg-green-medium' : 'bg-green-light' : i % 2 === 0 ? 'bg-green-light' : 'bg-green-medium'
+            icon: "",
+            color:
+              j % 2 === 0
+                ? i % 2 === 0
+                  ? "bg-green-medium"
+                  : "bg-green-light"
+                : i % 2 === 0
+                ? "bg-green-light"
+                : "bg-green-medium",
           });
         }
       }
@@ -146,66 +159,54 @@ export default defineComponent({
 
     // Função para colocar bandeiras onde tem bombas
     flagButton() {
-      if (this.flagButtonProps.color == 'grey-5') {
-        this.flagButtonProps.color = 'red'
-        this.flagButtonProps.icon = 'timer'
-        this.flagSwitch = true
+      if (this.flagButtonProps.color == "grey-5") {
+        this.flagButtonProps.color = "red";
+        this.flagButtonProps.icon = "timer";
+        this.flagSwitch = true;
       } else {
-        this.flagButtonProps.color = 'grey-5'
-        this.flagButtonProps.icon = 'flag'
-        this.flagSwitch = false
+        this.flagButtonProps.color = "grey-5";
+        this.flagButtonProps.icon = "flag";
+        this.flagSwitch = false;
       }
     },
 
     // Função atribuida aos botoes do campo para realizar as ações do jogo
     buttonFunction(row, col) {
-      let button = this.rowsButtons[row].buttons[col]
+      let button = this.rowsButtons[row].buttons[col];
 
-      if (this.flagSwitch == true && button.icon == 'flag') {
-        button.icon = ''
-        this.bombCount--
-      } else if (this.flagSwitch == true && button.icon == '') {
-        button.icon = 'flag'
-        this.bombCount++
+      if (this.flagSwitch == true && button.icon == "flag") {
+        button.icon = "";
+        this.bombCount--;
+      } else if (this.flagSwitch == true && button.icon == "") {
+        button.icon = "flag";
+        this.bombCount++;
       }
 
-      if (this.flagSwitch == false) {this.revealTiles(row, col)}
+      if (this.flagSwitch == false) {
+        this.revealTiles(row, col);
+      }
     },
 
     revealTiles(row, col) {
-      let button = this.rowsButtons[row].buttons[col]
-      let coord = this.game.board[row][col]
-      if (coord == 'M') {
+      let button = this.rowsButtons[row].buttons[col];
+      let coord = this.game.board[row][col];
+      if (coord == "M") {
         gameOverToast();
-        return
+        return;
       }
-      if (button.color == 'bg-green-light') {
-        button.color = 'bg-beige-light'
-      } else if (button.color == 'bg-green-medium'){
-        button.color = 'bg-beige-medium'
+      if (button.color == "bg-green-light") {
+        button.color = "bg-beige-light";
+      } else if (button.color == "bg-green-medium") {
+        button.color = "bg-beige-medium";
       }
-      if (button.icon == 'flag'){button.icon = ''; this.bombCount--}
+      if (button.icon == "flag") {
+        button.icon = "";
+        this.bombCount--;
+      }
       if (coord != 0) {
-        button.icon = this.iconNumbers[coord]
+        button.icon = this.iconNumbers[coord];
       }
-      // else {
-      //   this.searchForEmptyTiles(row, col)
-      // }
     },
-
-    // searchForEmptyTiles(row, col){
-    //   for (let index = 0; index < this.game.cellAroundingMine.length; index++) {
-    //     let newRow = row + this.game.cellAroundingMine[index][0];
-    //     let newCol = col + this.game.cellAroundingMine[index][1];
-    //     console.log(newRow);
-    //     console.log(newCol);
-    //     if (newRow >=0 && newCol >= 0) {
-    //       if (this.game.board[newRow][newCol] === 0) {
-    //         this.revealTiles(newRow,newCol)
-    //       }
-    //     }
-    //   }
-    // },
 
     // Função que está atribuída ao botão -começar- para inicializar um novo jogo
     init() {
@@ -220,21 +221,13 @@ export default defineComponent({
       this.startButton.disable = true;
       this.startButton.label = "Continuar";
       this.startButton.color = "secondary";
-    },
 
-    // Função que printa o array da lógica do jogo na tela apagar ou comentar posteriormente
-    // showBoard() {
-    //   let $board = document.getElementById('board')
-    //   for (let y = 0; y < this.game.board.length; y++) {
-    //     let $row = document.createElement('DIV');
-    //     for (let x = 0; x < this.game.board[y].length; x++) {
-    //         let $cell = document.createElement('BUTTON');
-    //         $cell.innerHTML = this.game.board[y][x];
-    //         $row.appendChild($cell);
-    //     }
-    //     $board.appendChild($row);
-    //   }
-    // },
+      this.timer = setInterval(() => {
+        // if (this.time > 0) {
+        this.time++;
+        // }
+      }, 1000);
+    },
 
     // Funções relativas a lógica do campo minado
     generateEmptyBoard() {
@@ -297,7 +290,7 @@ export default defineComponent({
         }
       }
     },
-    
+
     handleClickHome() {
       Swal.fire(
         swalConfs({
@@ -316,7 +309,7 @@ export default defineComponent({
         if (result.isConfirmed) {
           console.log("Confirmando");
         } else if (result.isDenied) {
-          window.location.href = '/'
+          window.location.href = "/";
           console.log("Negado");
         }
       });
